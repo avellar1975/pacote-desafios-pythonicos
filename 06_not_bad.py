1dda1010963eb9abc111b1715c0fb2b9838b2eb6
Evandro Avellar <evandro.avellar@gmail.com>
2021-06-05 08:20:23 -0300

    Solução exercicio 6

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -10,8 +10,12 @@ Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 """
 
 def not_bad(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    bad_s = s.find('bad')
+    not_s = s.find('not')
+    if bad_s > not_s:
+        return s.replace(s[not_s:bad_s + 3],'good')
+    else:
+        return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
